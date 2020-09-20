7f683845dddb4e629d104e72878d10c0f5ae4c4a
Sai Kshitij <saikshitij07@gmail.com>
2020-09-20 21:00:54 +0530

    Created models for Boards

--- backend/boards/admin.py
+++ backend/boards/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Board, List, Item, Label, Comment, Attachment
 
-# Register your models here.
+admin.site.register(Board)
+admin.site.register(List)
+admin.site.register(Item)
+admin.site.register(Label)
+admin.site.register(Comment)
+admin.site.register(Attachment)
--- backend/boards/models.py
+++ backend/boards/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils import timezone
+from users.models import User
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=False)
+    image = models.ImageField(blank=True, upload_to='board_images')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class List(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="lists")
+    title = models.CharField(max_length=255, blank=False, null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class Item(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=False)
+    image = models.ImageField(blank=True, upload_to='item_images')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class Label(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='labels')
+    title = models.CharField(max_length=255, blank=False, null=False)
+    color = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='comments')
+    description = models.TextField(blank=True, null=False)
+
+class Attachment(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='attachments')
+    upload = models.FileField(upload_to='attachments')
