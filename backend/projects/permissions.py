f2a99a3000273aee4afe672034725b9f5a09e986
Vikhyat <vdevired@usc.edu>
2020-09-23 15:21:37 +0530

    Cleanup

--- backend/projects/permissions.py
+++ backend/projects/permissions.py
@@ -13,3 +13,12 @@ class IsProjectAdminOrMemberReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user == obj.owner or pmem.access_level == 2
+
+class IsProjectMember(permissions.BasePermission):
+    try: 
+        pmem = ProjectMembership.objects.get(member=request.user, project=obj)
+    except ProjectMembership.DoesNotExist:
+        if request.user != obj.owner:
+            return False
+
+    return True
